ee0b0fb0fd6b9f34f08cf1236bbac2b273b7c052
Olgerd <bykalgerd@gmail.com>
2018-07-06 12:24:19 +0300

    add SelectCountryActivity

--- app/src/main/java/com/alherd/greatpeopleapp/MainActivity.java
+++ app/src/main/java/com/alherd/greatpeopleapp/MainActivity.java
@@ -1,13 +1,77 @@
 package com.alherd.greatpeopleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button writersButton;
+    Button artistsButton;
+    Button scientistsButton;
+    Button sportsmenButton;
+    Button politicsButton;
+    Button singersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        writersButton = (Button) findViewById(R.id.writers_button);
+        artistsButton = (Button) findViewById(R.id.artists_button);
+        scientistsButton = (Button) findViewById(R.id.scientists_button);
+        sportsmenButton = (Button) findViewById(R.id.sportsmen_button);
+        politicsButton = (Button) findViewById(R.id.polititions_button);
+        singersButton = (Button) findViewById(R.id.singers_button);
+
+        writersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectCountryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        artistsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectCountryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        scientistsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectCountryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sportsmenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectCountryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        politicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        singersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectCountryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/alherd/greatpeopleapp/SelectCountryActivity.java
@@ -0,0 +1,13 @@
+package com.alherd.greatpeopleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_country);
+    }
+}
